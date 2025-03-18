5505ea02ef1695a4b5eeeb4144b9d989cc09363f
Jericho Calumpiano <jcalumpiano@svi.com.ph>
2025-03-18 22:25:45 +0800

    Add player movement to react project

--- sokoban-react/src/App.js
+++ sokoban-react/src/App.js
@@ -32,9 +32,11 @@ const gridSize = 30;
 
 function GameArea() {
   const canvasRef = useRef(null);
+  const gameInitialized = useRef(false);
   let [hasPlayerWon, setHasPlayerWon] = useState(false);
   
   useEffect(() => {
+    if (gameInitialized.current) return;
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
     // let gridSize = 20;
@@ -44,25 +46,27 @@ function GameArea() {
       canvas.width = levels[0].canvasWidth * gridSize;
       canvas.height = levels[0].canvasHeight * gridSize;
       loadWallsFromTemplate(levels[0].template, context);
-      // updateGameArea();
+      updateGameArea(context);
     }
 
     window.addEventListener("keydown", function(event){
       if(!hasPlayerWon){
-          // updateGameArea()
-          canvas.key = event.key;
+          updateGameArea(context)
+          context.key = event.key;
+
       }
     });
     window.addEventListener("keyup", function(){
         if(!hasPlayerWon){
-            // updateGameArea()
-            canvas.key = false;
+            updateGameArea(context)
+            context.key = false;
         }
     })
-
     startGame();
+    gameInitialized.current = true;
     drawWalls();
   }, [])
+
 
 
   return (
@@ -91,7 +95,6 @@ function loadWallsFromTemplate(template, context) {
 }
 
 
-
 function drawWalls() {
   for (let wallBlock of wallBlocks) {
       wallBlock.drawWallBlock();
@@ -99,6 +102,52 @@ function drawWalls() {
   player.update();
 }
 
+function updateGameArea(context) {
+  clearCanvas(context);
+  let playerMoved = movePlayer(context);
+  if (playerMoved){
+    console.log("player moved")
+      // moveCounter.incrementMove()
+  }
+  drawWalls();
+  // checkWin();
+  // disableCurrentLevelButton();
+} 
+
+function clearCanvas(context) {
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clear the entire canvas
+}
+
+function movePlayer(context){
+  let playerMoved = false;
+  let initialX = player.x;
+  let initialY = player.y;
+  let newX = player.x;
+  let newY = player.y;
+
+  let movement = ""
+
+  if (context.key && context.key == "ArrowLeft") {newX -= player.speedX; movement = "left"}
+  if (context.key && context.key == "ArrowRight") {newX += player.speedX; movement = "right"}
+  if (context.key && context.key == "ArrowUp") {newY -= player.speedY; movement = "up"}
+  if (context.key && context.key == "ArrowDown") {newY += player.speedY; movement = "down"}
+
+  player.x = newX;
+  player.y = newY;
+
+  player.x = Math.max(0, Math.min(player.x, context.canvas.width - player.size))
+  player.y = Math.max(0, Math.min(player.y, context.canvas.height - player.size))
+
+  player.update();
+
+  if(initialX != player.x || initialY != player.y){
+    playerMoved = true;
+  }else{
+      playerMoved = false;
+  }
+  return playerMoved
+
+}
 
 function MainArea() {
   return (
